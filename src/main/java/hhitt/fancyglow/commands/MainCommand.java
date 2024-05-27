18b0e81da9f2c8979a740ca092cfe7deab2c899e
hhitt <diegovicentehernandez276@gmail.com>
2024-05-27 15:53:37 +0200

    Added permission fancyglow.command.disable

--- src/main/java/hhitt/fancyglow/commands/MainCommand.java
+++ src/main/java/hhitt/fancyglow/commands/MainCommand.java
@@ -30,48 +30,53 @@ public class MainCommand implements CommandExecutor {
         List<String> noAllowedWorlds = plugin.getConfig().getStringList("Disabled_Worlds");
 
 
-        //Users commands
+        //Users commands (/glow) that open the gui
+
         if (!sender.hasPermission("fancyglow.command")) {
             MessageUtils.miniMessageSender((Player) sender, plugin.getMainConfigManager().getNoPermissionMessage());
             return true;
-        } else{
+        }
 
-            //Logic for /glow
-            if(args == null || args.length == 0){
+        if(args == null || args.length == 0){
 
-                //Check the world
-                if (noAllowedWorlds.contains(actualWorld)) {
-                    MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getDisabledWorldMessage());
-                    return true;
-                }
-
-                //Open the gui
-                CreatingInventory inventory = new CreatingInventory(plugin, (Player) sender);
-                ((Player) sender).openInventory(inventory.getInventory());
-                return true;
-
-            }
-
-            //Logic for /glow disable
-            if(args[0].equalsIgnoreCase("disable")){
-                Scoreboard scoreboard = ((Player) sender).getScoreboard();
-
-                for (Team team : scoreboard.getTeams()) {
-                    if (team.hasEntry(player.getName())) {
-                        team.removeEntry(player.getName());
-                    }
-                }
-
-                //If the player is not glowing, "exception" error
-                if(!player.isGlowing()){
-                    MessageUtils.miniMessageSender(player, plugin.getConfig().getString("Messages.Not_Glowing"));
-                }else{
-                    //If is glowing, then disabled message
-                    MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getDisableGlow());
-                }
-                ((Player) sender).setGlowing(false);
+            //Check the world
+            if (noAllowedWorlds.contains(actualWorld)) {
+                MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getDisabledWorldMessage());
                 return true;
             }
+
+            //Open the gui
+            CreatingInventory inventory = new CreatingInventory(plugin, (Player) sender);
+            ((Player) sender).openInventory(inventory.getInventory());
+            return true;
+
+        }
+
+
+        //Logic for /glow disable
+        if(!sender.hasPermission("fancyglow.command.disable")){
+            MessageUtils.miniMessageSender((Player) sender, plugin.getMainConfigManager().getNoPermissionMessage());
+            return true;
+        }
+
+        if(args[0].equalsIgnoreCase("disable")){
+            Scoreboard scoreboard = ((Player) sender).getScoreboard();
+
+            for (Team team : scoreboard.getTeams()) {
+                if (team.hasEntry(player.getName())) {
+                    team.removeEntry(player.getName());
+                }
+            }
+
+            //If the player is not glowing, "exception" error
+            if(!player.isGlowing()){
+                MessageUtils.miniMessageSender(player, plugin.getConfig().getString("Messages.Not_Glowing"));
+            }else{
+                //If is glowing, then disabled message
+                MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getDisableGlow());
+            }
+            ((Player) sender).setGlowing(false);
+            return true;
         }
 
 
