bce77f8f8a05ae83740ac051b8ffb22eb2bf2a05
hhitt <diegovicentehernandez276@gmail.com>
2024-07-01 14:27:26 +0200

    fix: spigot compatibility

--- src/main/java/hhitt/fancyglow/commands/ColorCommandLogic.java
+++ src/main/java/hhitt/fancyglow/commands/ColorCommandLogic.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
+import org.jetbrains.annotations.NotNull;
 
 public class ColorCommandLogic implements CommandExecutor {
     private final FancyGlow plugin;
@@ -20,7 +21,7 @@ public class ColorCommandLogic implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 1) {
