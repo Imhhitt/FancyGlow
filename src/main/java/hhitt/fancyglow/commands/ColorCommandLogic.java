a8fd5edf4f32c27b99c387c840f560f6d00f2984
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-07-03 12:30:02 -0400

    refactor: Early return instead of if else, remove local method and use it from GlowManager again.

--- src/main/java/hhitt/fancyglow/commands/ColorCommandLogic.java
+++ src/main/java/hhitt/fancyglow/commands/ColorCommandLogic.java
@@ -8,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 
 public class ColorCommandLogic implements CommandExecutor {
@@ -38,44 +37,29 @@ public class ColorCommandLogic implements CommandExecutor {
 
         if (arg.equals("rainbow")) {
             if (!(
-                p.hasPermission("fancyglow.rainbow") ||
-                p.hasPermission("fancyglow.all_colors")
+                    p.hasPermission("fancyglow.rainbow") ||
+                            p.hasPermission("fancyglow.all_colors")
             )) {
                 MessageUtils.miniMessageSender(p, plugin.getMainConfigManager().getNoPermissionMessage());
                 return;
             }
             glowManager.toggleMulticolorGlow(p);
             return;
-        } else {
-            try {
-                color = ChatColor.valueOf(arg.toUpperCase());
-            } catch (IllegalArgumentException e) {
-                MessageUtils.miniMessageSender(p, plugin.getConfig().getString("Messages.Not_Valid_Color"));
-                return;
-            }
+        }
+
+        try {
+            color = ChatColor.valueOf(arg.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            MessageUtils.miniMessageSender(p, plugin.getConfig().getString("Messages.Not_Valid_Color"));
+            return;
         }
 
         if (!(glowManager.hasGlowPermission(p, color) || p.hasPermission("fancyglow.all_colors") || p.hasPermission("fancyglow.admin"))) {
             MessageUtils.miniMessageSender(p, plugin.getMainConfigManager().getNoPermissionMessage());
-        } else {
-            toggleGlow(p, color);
+            return;
         }
+
+        glowManager.toggleGlow(p, color);
     }
 
-    private void toggleGlow(Player player, ChatColor color) {
-        Team glowTeam = glowManager.getOrCreateTeam(color);
-        if (glowTeam != null) {
-            String cleanName = ChatColor.stripColor(player.getName());
-            if (glowTeam.hasEntry(cleanName)) {
-                glowTeam.removeEntry(cleanName);
-                player.setGlowing(false);
-                MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getDisableGlow());
-            } else {
-                glowManager.removePlayerFromAllTeams(player);
-                glowTeam.addEntry(cleanName);
-                player.setGlowing(true);
-                MessageUtils.miniMessageSender(player, plugin.getMainConfigManager().getEnableGlow());
-            }
-        }
-    }
 }
