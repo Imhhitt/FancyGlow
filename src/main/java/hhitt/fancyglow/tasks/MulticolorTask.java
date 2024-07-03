50dbe05ddbb5ebf46ff9ad6186e3b37109d892a3
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-07-03 12:20:58 -0400

    refactor: Removed local methods and using them from GlowManager.

--- src/main/java/hhitt/fancyglow/tasks/MulticolorTask.java
+++ src/main/java/hhitt/fancyglow/tasks/MulticolorTask.java
@@ -1,10 +1,10 @@
 package hhitt.fancyglow.tasks;
 
 import hhitt.fancyglow.FancyGlow;
+import hhitt.fancyglow.utils.GlowManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public class MulticolorTask extends BukkitRunnable {
@@ -13,11 +13,13 @@ public class MulticolorTask extends BukkitRunnable {
     private final Player player;
     private final ChatColor[] colorArray;
     private int currentIndex;
+    private final GlowManager glowManager;
 
     public MulticolorTask(FancyGlow plugin, Player player) {
         this.plugin = plugin;
         this.player = player;
         this.colorArray = getAllColors();
+        this.glowManager = plugin.getGlowManager();
         this.currentIndex = 0;
     }
 
@@ -54,27 +56,11 @@ public class MulticolorTask extends BukkitRunnable {
     }
 
     private ChatColor[] getAllColors() {
-        return new ChatColor[] {
+        return new ChatColor[]{
                 ChatColor.BLACK, ChatColor.DARK_BLUE, ChatColor.DARK_GREEN, ChatColor.DARK_AQUA,
                 ChatColor.DARK_RED, ChatColor.DARK_PURPLE, ChatColor.GOLD, ChatColor.GRAY,
                 ChatColor.DARK_GRAY, ChatColor.BLUE, ChatColor.GREEN, ChatColor.AQUA,
                 ChatColor.RED, ChatColor.LIGHT_PURPLE, ChatColor.YELLOW, ChatColor.WHITE
         };
-    }
-
-    private Team getOrCreateTeam(ChatColor color) {
-        Scoreboard board = plugin.getServer().getScoreboardManager().getMainScoreboard();
-        Team glowTeam = board.getTeam(color.name());
-        if (glowTeam == null) {
-            glowTeam = createTeam(color);
-        }
-        return glowTeam;
-    }
-
-    private Team createTeam(ChatColor color) {
-        Scoreboard board = plugin.getServer().getScoreboardManager().getMainScoreboard();
-        Team team = board.registerNewTeam(color.name());
-        team.setColor(color);
-        return team;
     }
 }
