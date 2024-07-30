2a0d885b91265eaf5e8d6ab8a01241c1fa994cd4
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-07-30 10:37:11 -0400

    refactor: Make MenuClickListener use ColorUtils.

--- src/main/java/hhitt/fancyglow/listeners/MenuClickListener.java
+++ src/main/java/hhitt/fancyglow/listeners/MenuClickListener.java
@@ -2,10 +2,10 @@ package hhitt.fancyglow.listeners;
 
 import hhitt.fancyglow.FancyGlow;
 import hhitt.fancyglow.inventory.CreatingInventory;
-import hhitt.fancyglow.utils.GlowManager;
+import hhitt.fancyglow.managers.GlowManager;
+import hhitt.fancyglow.utils.ColorUtils;
 import hhitt.fancyglow.utils.MessageUtils;
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,35 +15,10 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MenuClickListener implements Listener {
 
     private final FancyGlow plugin;
     private final GlowManager glowManager;
-
-    // Implementación para la lógica para mapear los colores de la armadura de cuero a los colores.
-    private static final Map<Color, ChatColor> colorMap = new HashMap<>();
-
-    static {
-        colorMap.put(Color.BLACK, ChatColor.BLACK);
-        colorMap.put(Color.BLUE, ChatColor.BLUE);
-        colorMap.put(Color.LIME, ChatColor.GREEN);
-        colorMap.put(Color.AQUA, ChatColor.AQUA);
-        colorMap.put(Color.RED, ChatColor.RED);
-        colorMap.put(Color.FUCHSIA, ChatColor.LIGHT_PURPLE);
-        colorMap.put(Color.YELLOW, ChatColor.YELLOW);
-        colorMap.put(Color.WHITE, ChatColor.WHITE);
-        colorMap.put(Color.SILVER, ChatColor.GRAY);
-        colorMap.put(Color.GRAY, ChatColor.DARK_GRAY);
-        colorMap.put(Color.NAVY, ChatColor.DARK_BLUE);
-        colorMap.put(Color.GREEN, ChatColor.DARK_GREEN);
-        colorMap.put(Color.TEAL, ChatColor.DARK_AQUA);
-        colorMap.put(Color.MAROON, ChatColor.DARK_RED);
-        colorMap.put(Color.PURPLE, ChatColor.DARK_PURPLE);
-        colorMap.put(Color.ORANGE, ChatColor.GOLD);
-    }
 
     public MenuClickListener(FancyGlow plugin) {
         this.plugin = plugin;
@@ -75,7 +50,7 @@ public class MenuClickListener implements Listener {
             return;
         }
 
-        ChatColor color = getColorFromArmorColor(meta.getColor());
+        ChatColor color = ColorUtils.getColorFromArmorColor(meta.getColor());
         if (!(color != null && glowManager.hasGlowPermission(p, color) ||
                 color != null && p.hasPermission("fancyglow.all_colors") ||
                 color != null && p.hasPermission("fancyglow.admin"))) {
@@ -86,9 +61,5 @@ public class MenuClickListener implements Listener {
 
         glowManager.toggleGlow(p, color);
         p.closeInventory();
-    }
-
-    private ChatColor getColorFromArmorColor(org.bukkit.Color armorColor) {
-        return colorMap.getOrDefault(armorColor, ChatColor.WHITE);
     }
 }
