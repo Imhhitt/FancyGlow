0a32b7f0aff98e37a1926a6d95a7029e67d936dd
mrlu <75042214+dxvdc@users.noreply.github.com>
2025-02-01 16:28:43 +0100

    🔥 Optimized GlowEffectManager – Better Performance & Stability
    ✔ More efficient team management 🏎
    ✔ More stable and bug-free code 🛡
    ✔ Better compatibility with colors 🎨

--- src/main/java/hhitt/fancyglow/managers/GlowEffectManager.java
+++ src/main/java/hhitt/fancyglow/managers/GlowEffectManager.java
@@ -5,63 +5,64 @@ import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerTe
 import hhitt.fancyglow.utils.MessageUtils;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-// TODO: Proper implementation.
+import java.util.Optional;
+
 public class GlowEffectManager {
 
     private static final String TEAM_NAME_PREFIX = "glow_";
 
     public static void setPlayerGlow(Player player, NamedTextColor color) {
-        String teamName = TEAM_NAME_PREFIX + player.getName();
         Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-        Team team = scoreboard.getTeam(teamName);
+        String teamName = generateTeamName(player);
+        
+        Team team = Optional.ofNullable(scoreboard.getTeam(teamName))
+                .orElseGet(() -> scoreboard.registerNewTeam(teamName));
 
-        if (team == null) {
-            team = scoreboard.registerNewTeam(teamName);
-        }
-
-        team.setColor(org.bukkit.ChatColor.valueOf(color.toString()));
+        team.setColor(convertColor(color));
         team.addEntry(player.getName());
         team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
         team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
+        sendGlowPacket(player, color, WrapperPlayServerTeams.OptionData.ALL);
+    }
+
+    public static void removePlayerGlow(Player player) {
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+        String teamName = generateTeamName(player);
+        Optional.ofNullable(scoreboard.getTeam(teamName)).ifPresent(team -> {
+            team.removeEntry(player.getName());
+            team.unregister();
+            sendGlowPacket(player, NamedTextColor.WHITE, WrapperPlayServerTeams.OptionData.NONE);
+        });
+    }
+
+    private static void sendGlowPacket(Player player, NamedTextColor color, WrapperPlayServerTeams.OptionData option) {
         WrapperPlayServerTeams.ScoreBoardTeamInfo teamInfo = new WrapperPlayServerTeams.ScoreBoardTeamInfo(
-                MessageUtils.parse(teamName),
+                MessageUtils.parse(generateTeamName(player)),
                 null,
                 null,
                 WrapperPlayServerTeams.NameTagVisibility.ALWAYS,
                 WrapperPlayServerTeams.CollisionRule.ALWAYS,
                 color,
-                WrapperPlayServerTeams.OptionData.ALL
+                option
         );
-
         PacketEvents.getAPI().getPlayerManager().sendPacket(player, teamInfo);
     }
 
-    public static void removePlayerGlow(Player player) {
-        String teamName = TEAM_NAME_PREFIX + player.getName();
-        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-        Team team = scoreboard.getTeam(teamName);
-
-        if (team != null) {
-            team.removeEntry(player.getName());
-
-            WrapperPlayServerTeams.ScoreBoardTeamInfo teamInfo = new WrapperPlayServerTeams.ScoreBoardTeamInfo(
-                    MessageUtils.parse(teamName),
-                    null,
-                    null,
-                    WrapperPlayServerTeams.NameTagVisibility.ALWAYS,
-                    WrapperPlayServerTeams.CollisionRule.ALWAYS,
-                    NamedTextColor.WHITE,
-                    WrapperPlayServerTeams.OptionData.NONE
-            );
-
-            PacketEvents.getAPI().getPlayerManager().sendPacket(player, teamInfo);
-
-            team.unregister();
+    private static ChatColor convertColor(NamedTextColor color) {
+        try {
+            return ChatColor.valueOf(color.toString().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return ChatColor.WHITE;
         }
+    }
+
+    private static String generateTeamName(Player player) {
+        return TEAM_NAME_PREFIX + player.getUniqueId().toString().substring(0, 8);
     }
 }
