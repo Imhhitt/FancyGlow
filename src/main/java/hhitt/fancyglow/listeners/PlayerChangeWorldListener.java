61d3d0400b21a3a69da5dc4b7d8d50861a612daf
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2025-03-05 00:14:10 -0300

    fix(PlayerChangeWorldListener): Only notify player the disabled world message if has a glow mode on.

--- src/main/java/hhitt/fancyglow/listeners/PlayerChangeWorldListener.java
+++ src/main/java/hhitt/fancyglow/listeners/PlayerChangeWorldListener.java
@@ -2,6 +2,7 @@ package hhitt.fancyglow.listeners;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
 import hhitt.fancyglow.FancyGlow;
+import hhitt.fancyglow.managers.PlayerGlowManager;
 import hhitt.fancyglow.utils.MessageHandler;
 import hhitt.fancyglow.utils.Messages;
 import org.bukkit.entity.Player;
@@ -13,17 +14,19 @@ public class PlayerChangeWorldListener implements Listener {
 
     private final YamlDocument config;
     private final MessageHandler messageHandler;
+    private final PlayerGlowManager playerGlowManager;
 
     public PlayerChangeWorldListener(FancyGlow plugin) {
         this.config = plugin.getConfiguration();
         this.messageHandler = plugin.getMessageHandler();
+        this.playerGlowManager = plugin.getPlayerGlowManager();
     }
 
     @EventHandler
     public void onPlayerWorldChange(PlayerChangedWorldEvent e) {
         Player player = e.getPlayer();
         String worldName = player.getWorld().getName();
-        if (config.getStringList("Disabled_Worlds").contains(worldName)) {
+        if (config.getStringList("Disabled_Worlds").contains(worldName) && !playerGlowManager.getPlayerGlowingMode(player).equalsIgnoreCase("NONE")) {
             player.setGlowing(false);
             messageHandler.sendMessage(player, Messages.DISABLED_WORLD);
         }
